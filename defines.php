d211c5567150e7dd8c435d4a3938ccdab13ad8c3
Matthijs Alles <admin@bixie.nl>
2014-01-01 16:37:30 +0100

    bps defines
    classes definen voor sessie start

---
+++ defines.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *	com_bixprintshop - Online-PrintStore for Joomla
+ *	Copyright (C) 2011-2014 Matthijs Alles
+ *	Bixie.nl
+ *
+ */
+
+if (file_exists(dirname(__FILE__).'/administrator/components/com_bixprintshop/defines.php')) {
+	require_once dirname(__FILE__).'/administrator/components/com_bixprintshop/defines.php';
+}
