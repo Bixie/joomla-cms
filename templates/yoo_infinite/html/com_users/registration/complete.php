f7e7ec2b759e927ab30bf6f4db3ae1b7308ad2b0
Matthijs Alles <admin@bixie.nl>
2014-01-06 13:01:10 +0100

    user registration templates

---
+++ templates/yoo_infinite/html/com_users/registration/complete.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @package   yoo_solar
+* @author    YOOtheme http://www.yootheme.com
+* @copyright Copyright (C) YOOtheme GmbH
+* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+*/
+?>
+<div id="system">
+	<h1 class="title">ACCOUNT AANGEVRAAGD</h1>
+	<p>Bedankt voor het aanvragen van het account.</p>
+
+    <a href="/" class="button-default">Homepage</a>
+</div>
